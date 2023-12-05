22278041a1587d56b5f31b87644edd42e2ccc307
suskidee <109852676+suskidee@users.noreply.github.com>
2023-12-05 17:45:05 +0100

    fourth commit

--- contactapi/settings.py
+++ contactapi/settings.py
@@ -145,6 +145,4 @@ ALGORITHM = os.environ.get('ALGORITHM')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-]
+CORS_ALLOWED_ORIGINS = ["*"]
